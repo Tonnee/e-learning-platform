8e0838bfef1792f99c51d66ff7b509a744392f79
Sabiha Afrin <satonnee@gmail.com>
2022-11-18 18:30:08 +0600

    Side Navigation bar update

--- script/script.js
+++ script/script.js
@@ -73,3 +73,22 @@ $('.outline_slider').slick({
     autoplaySpeed: 5000,
     pauseOnHover: true
 });
+// jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
+
+var items = $(".list-wrapper .list-item");
+    var numItems = items.length;
+    var perPage = 4;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
