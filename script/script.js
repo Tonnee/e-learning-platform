5c2f492e5716471feb9f3f9d57bc8b5d43519c62
Sabiha Afrin <satonnee@gmail.com>
2022-11-19 00:08:45 +0600

    Free question page update

--- script/script.js
+++ script/script.js
@@ -76,19 +76,23 @@ $('.outline_slider').slick({
 // jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
 
 var items = $(".list-wrapper .list-item");
-    var numItems = items.length;
-    var perPage = 4;
+var numItems = items.length;
+var perPage = 5;
 
-    items.slice(perPage).hide();
+items.slice(perPage).hide();
 
-    $('#pagination-container').pagination({
-        items: numItems,
-        itemsOnPage: perPage,
-        prevText: "&laquo;",
-        nextText: "&raquo;",
-        onPageClick: function (pageNumber) {
-            var showFrom = perPage * (pageNumber - 1);
-            var showTo = showFrom + perPage;
-            items.hide().slice(showFrom, showTo).show();
-        }
-    });
+$('#pagination-container').pagination({
+    items: numItems,
+    itemsOnPage: perPage,
+    prevText: "&laquo;",
+    nextText: "&raquo;",
+    onPageClick: function (pageNumber) {
+        var showFrom = perPage * (pageNumber - 1);
+        var showTo = showFrom + perPage;
+        items.hide().slice(showFrom, showTo).show();
+        $('body,html').animate({
+            scrollTop :0
+        }, 100);
+    }
+});
+
